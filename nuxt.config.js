7489ec6130472ca36e45ec45b5cf133a2020064a
Kaiyu <anne550323@gmail.com>
2022-02-07 11:40:29 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  buildModules: [
+    // Simple usage
+    "@nuxtjs/vuetify",
+
+    // With options
+    [
+      "@nuxtjs/vuetify",
+      {
+        /* module options */
+      },
+    ],
+  ],
+};
